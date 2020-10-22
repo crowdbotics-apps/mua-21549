d5ba51a734f9618c3dccd9cb242d9c47af067be1
Crowdbotics <team@crowdbotics.com>
2020-10-22 13:49:40 +0000

    Update components

--- src/features/BlankScreen0155493/screens/blank.js
+++ src/features/BlankScreen0155493/screens/blank.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from "react"
 import {
   View,
   Image,
@@ -8,33 +8,91 @@ import {
   Button,
   Switch,
   TextInput,
-  StyleSheet,
-} from "react-native";
-import DateTimePicker from 'react-native-datepicker';
-import Icon from 'react-native-vector-icons/FontAwesome';
-import Slider from '@react-native-community/slider';
-import { CheckBox } from 'react-native-elements';
-import {SlideMenuIcon} from '../../../navigator/slideMenuIcon';
+  StyleSheet
+} from "react-native"
+import DateTimePicker from "react-native-datepicker"
+import Icon from "react-native-vector-icons/FontAwesome"
+import Slider from "@react-native-community/slider"
+import { CheckBox } from "react-native-elements"
+import { SlideMenuIcon } from "../../../navigator/slideMenuIcon"
 
 export default class Blank extends React.Component {
-  static navigationOptions = ({navigation}) => {
+  static navigationOptions = ({ navigation }) => {
     return {
-      headerLeft: <SlideMenuIcon navigationProps={navigation} />,
-    };
-  };
-  
-  state = {};
+      headerLeft: <SlideMenuIcon navigationProps={navigation} />
+    }
+  }
+
+  state = {}
 
   render = () => (
-    <View style={styles.container}>
-      <Text>This is your new component</Text>
+    <View>
+      <View>
+        <View style={styles.View_10}>
+          <Icon name="align-justify" />
+        </View>
+      </View>
+      <View>
+        <View style={styles.View_20}>
+          <Text style={styles.Text_27}>M.U.A.</Text>
+          <Text style={styles.Text_29}>Making Up Acronyms</Text>
+        </View>
+      </View>
+      <Button
+        title="START GAME"
+        color="#030303"
+        style={styles.Button_5}
+        onPress={() => alert("Pressed!")}
+      />
+      <Button
+        title="JOIN GAME"
+        color="#ffffff"
+        style={styles.Button_21}
+        onPress={() => alert("Pressed!")}
+      />
     </View>
-  );
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    marginHorizontal: 16,
+    marginHorizontal: 16
   },
-});
+  View_1: {},
+  View_9: {},
+  View_10: { height: -11 },
+  Icon_25: {},
+  View_18: {},
+  View_20: { alignSelf: "center" },
+  Text_27: {
+    height: 100,
+    alignSelf: "center",
+    fontSize: 87,
+    color: "#b26262",
+    fontFamily: "Lato-Regular",
+    letterSpacing: 6
+  },
+  Text_29: {
+    alignSelf: "center",
+    fontFamily: "Lato-Regular",
+    textAlign: "center"
+  },
+  Button_5: {
+    width: "97%",
+    marginLeft: 3,
+    paddingTop: 14,
+    alignSelf: "center",
+    color: "#fcfcfc",
+    fontFamily: "Roboto-Bold",
+    letterSpacing: 4
+  },
+  Button_21: {
+    width: "97%",
+    marginLeft: 3,
+    paddingTop: 14,
+    alignSelf: "center",
+    fontFamily: "Roboto-Bold",
+    letterSpacing: 4
+  }
+})
